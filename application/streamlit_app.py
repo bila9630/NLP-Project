33babfc15d7fab47c7ca08730569676f7025f493
Viet Duc Kieu <viet.duc.kieu@web.de>
2023-01-19 15:08:04 +0100

    deploy example model2

--- application/streamlit_app.py
+++ application/streamlit_app.py
@@ -1,18 +1,39 @@
 import streamlit as st
 import random
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
 st.set_page_config(
     page_title="The fast reporters",
     page_icon="📰",
 )
 
 
-def random_class(title, description, model):
-    # pass title and description to model
-    # category = model.predict(title, description)
-    category = random.choice(
-        ["U.S. NEWS", "COMEDY", "WORLD NEWS", "TECH", "ENVIRONMENT"])
+# Load the tokenizer and model
+with open("tokenizer.pickle", "rb") as handle:
+    tokenizer = pickle.load(handle)
+
+with open("model.pickle", "rb") as handle:
+    model = pickle.load(handle)
+
+
+def random_class(title, description):
+    # Concatenate title and description to create a single input text
+    text = title + " " + description
+
+    # Convert the input text to numerical sequences
+    input_string = [text]
+    input_string = pad_sequences(
+        tokenizer.texts_to_sequences(input_string), maxlen=140)
+
+    # Predict the category using the input text
+    predicted_category = model.predict(input_string)
+
+    # Generate a random confidence value
     confidence = round(random.random(), 2)
-    return category, confidence
+
+    return predicted_category, confidence
 
 
 st.header("Welcome to the fast reporters!")
@@ -31,7 +52,7 @@ news_description = st.text_area(label='Newspaper description',
 
 if st.button('Classify'):
     category, confidence = random_class(
-        news_title, news_description, model_selectbox)
+        news_title, news_description)
     st.write("Model: ", model_selectbox)
     st.write(f"Category: {category}")
-    st.write(f"Confidence: {confidence}")
+    st.write(f"Confidence: {confidence} (random for now)")
