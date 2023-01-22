9a25c9aa0b066ac19c95a9c830f483b4571c6f14
Viet Duc Kieu <viet.duc.kieu@web.de>
2023-01-22 15:50:55 +0100

    fix page icon

--- application/streamlit_app.py
+++ application/streamlit_app.py
@@ -9,7 +9,7 @@ from nltk.corpus import stopwords
 # set page config (must be called as the first Streamlit command)
 st.set_page_config(
     page_title="The fast reporters",
-    page_icon=":books:",
+    page_icon=":newspaper:",
 )
 
 
