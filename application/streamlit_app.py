a0b648bf3ae067cb656b01c35a5ce31ff50ffdb5
Viet Duc Kieu <viet.duc.kieu@web.de>
2023-01-22 09:33:39 +0100

    deploy model

--- application/streamlit_app.py
+++ application/streamlit_app.py
@@ -1,24 +1,62 @@
 import streamlit as st
+import re
 import random
 import pickle
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
+nltk.download("stopwords")
+nltk.download("punkt")
 
 st.set_page_config(
     page_title="The fast reporters",
     page_icon="📰",
 )
 
+# Load the model
+with open("naivebayes.pkl", "rb") as handle:
+    model = pickle.load(handle)
 
-# with open("model.pickle", "rb") as handle:
-#     model = pickle.load(handle)
+# Preprocess text
 
 
-def random_class(title, description):
+def process_text(text):
+    # convert text to lowercase, remove newlines and carriage returns, and strip leading/trailing whitespace
+    text = text.lower().replace('\n', ' ').replace('\r', '').strip()
+
+    # replace multiple spaces with single space
+    text = re.sub(' +', ' ', text)
+
+    # remove non-alphanumeric characters and digits
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r'[0-9]', '', text)
+
+    # create set of english stopwords
+    stop_words = set(stopwords.words('english'))
+
+    # tokenize text into words
+    word_tokens = word_tokenize(text)
+
+    # if word not in stops_words, add word to filtered_sentence
+    filtered_sentence = []
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+
+    text = " ".join(filtered_sentence)
+    return text
+
+
+def predict_category(title, description):
     # Concatenate title and description to create a single input text
     text = title + " " + description
+    text_processed = process_text(text)
 
     # Predict the category using the input text
-    predicted_category = text
+    predicted_category = model.predict([text_processed])
+    # Convert the predicted category to a string
+    predicted_category = predicted_category[0]
 
     # Generate a random confidence value
     confidence = round(random.random(), 2)
@@ -31,7 +69,7 @@ st.write("Simply enter a news title and description and we'll classify it for yo
 # Create a selectbox widget for choosing a machine learning model
 model_selectbox = st.selectbox(
     "Choose a machine learning model:",
-    ["Decision Tree", "Random Forest", "SVM"]
+    ["Naives Bayes", "Random Forest", "Linear SVM"]
 )
 
 news_title = st.text_input(label='Newspaper title',
@@ -41,8 +79,7 @@ news_description = st.text_area(label='Newspaper description',
 
 
 if st.button('Classify'):
-    category, confidence = random_class(
-        news_title, news_description)
+    category, confidence = predict_category(news_title, news_description)
     st.write("Model: ", model_selectbox)
     st.write(f"Category: {category}")
     st.write(f"Confidence: {confidence} (random for now)")
