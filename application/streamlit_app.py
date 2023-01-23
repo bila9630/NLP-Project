0527a1b6ac499483a159fe94af94037288e2f9e6
Viet Duc Kieu <viet.duc.kieu@web.de>
2023-01-23 01:21:57 +0100

    add cnn

--- application/streamlit_app.py
+++ application/streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import re
-import random
 import pickle
 import numpy as np
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # set page config (must be called as the first Streamlit command)
 st.set_page_config(
@@ -37,13 +37,15 @@ def load_model_path():
 model_nb, model_svm = load_model_path()
 
 
-# import model on local machine
+# import model on local machine (UNCOMMENT BELOW WHEN RUNNING LOCAL)
 # model_nb = pickle.load(open("naiveBayes.pkl", "rb"))
 # model_svm = pickle.load(open("linearSVM.pkl", "rb"))
 
 # Load the saved model nn and tokenizer
+
+
 @st.cache(allow_output_mutation=True)
-def load_saved_model_nn(model_path, tokenizer_path, encoder_path):
+def load_saved_model(model_path, tokenizer_path, encoder_path):
     # Load the model from the file
     model = load_model(model_path)
 
@@ -57,12 +59,18 @@ def load_saved_model_nn(model_path, tokenizer_path, encoder_path):
 
     return model, tokenizer, encoder
 
+
 # Load the model and tokenizer for deployment
-model_nn, tokenizer_nn, encoder_nn = load_saved_model_nn("application/model_nn.h5", "application/tokenizer_nn.pickle", "application/encoder_nn.pickle")
+model_nn, tokenizer_nn, encoder_nn = load_saved_model(
+    "application/model_nn.h5", "application/tokenizer_nn.pickle", "application/encoder_nn.pickle")
+model_cnn, tokenizer_cnn, encoder_cnn = load_saved_model(
+    "application/model_cnn.h5", "application/tokenizer_cnn.pickle", "application/encoder_cnn.pickle")
 
-# Load the model and tokenizer on local machine
-# model_nn, tokenizer_nn, encoder_nn = load_saved_model_nn("model_nn.h5", "tokenizer_nn.pickle", "encoder_nn.pickle")
-
+# Load the model and tokenizer on local machine (UNCOMMENT BELOW WHEN RUNNING LOCAL)
+# model_nn, tokenizer_nn, encoder_nn = load_saved_model(
+#     "model_nn.h5", "tokenizer_nn.pickle", "encoder_nn.pickle")
+# model_cnn, tokenizer_cnn, encoder_cnn = load_saved_model(
+#     "model_cnn.h5", "tokenizer_cnn.pickle", "encoder_cnn.pickle")
 
 
 # Preprocess text function
@@ -93,13 +101,13 @@ def process_text(text):
     return text
 
 
-def predict_category(title, description):
-    # Concatenate title and description to create a single input text
-    text = title + " " + description
-    text_processed = process_text(text)
-
+def predict_nn(text):
+    '''
+    Predict the category of a news article using the neural network model
+    args: text (str): the text of the news article
+    '''
     # Predict the category with the neural network model
-    text_matrix_nn = tokenizer_nn.texts_to_matrix([text_processed])
+    text_matrix_nn = tokenizer_nn.texts_to_matrix([text])
     predictions_nn = model_nn.predict(text_matrix_nn)
 
     # Get the index of the class with the highest probability
@@ -110,13 +118,55 @@ def predict_category(title, description):
     # Get the predicted class probability
     predicted_class_prob = predictions_nn[0][predicted_class_index]
 
+    return predicted_class_label, predicted_class_prob
+
+
+def predict_cnn(text):
+    '''
+    Predict the category of a news article using the CNN model
+    args: text (str): the text of the news article
+    '''
+    # Predict the category with the CNN model
+    text_seq = tokenizer_cnn.texts_to_sequences([text])
+    text_padseq = pad_sequences(text_seq, maxlen=110)
+
+    predictions_cnn = model_cnn.predict(text_padseq)
+
+    # Get the index of the class with the highest probability
+    predicted_class_index = np.argmax(predictions_cnn)
+    # Get the corresponding class label from the label encoder
+    predicted_class_label = encoder_cnn.classes_[predicted_class_index]
+
+    # Get the predicted class probability
+    predicted_class_prob = predictions_cnn[0][predicted_class_index]
+
+    return predicted_class_label, predicted_class_prob
+
+
+def predict_category(title, description):
+    '''
+    Predict the category of a news article using all models
+    args:
+        - title (str): the title of the news article
+        - description (str): the description of the news article
+    returns:
+        - predictions (dict): a dictionary containing the predictions of all models
+    '''
+    # Concatenate title and description to create a single input text
+    text = title + " " + description
+    text_processed = process_text(text)
+
+    category_nn, prob_category_nn = predict_nn(text_processed)
+    category_cnn, prob_category_cnn = predict_cnn(text_processed)
 
     # Predict the category with both models and store the predictions
     predictions = {
         "naive_bayes": model_nb.predict([text_processed])[0],
         "svm": model_svm.predict([text_processed])[0],
-        "nn": predicted_class_label,
-        "nn_prob": predicted_class_prob
+        "nn": category_nn,
+        "nn_prob": prob_category_nn,
+        "cnn": category_cnn,
+        "cnn_prob": prob_category_cnn
     }
 
     return predictions
@@ -135,4 +185,7 @@ if st.button('Classify'):
     predictions = predict_category(news_title, news_description)
     st.write(f"Category Naives Bayes: {predictions['naive_bayes']}")
     st.write(f"Category SVM: {predictions['svm']}")
-    st.write(f"Category NN: {predictions['nn']}, with probability: {predictions['nn_prob']:.2f}")
+    st.write(
+        f"Category NN: {predictions['nn']}, with probability: {predictions['nn_prob']:.2f}")
+    st.write(
+        f"Category CNN: {predictions['cnn']}, with probability: {predictions['cnn_prob']:.2f}")
